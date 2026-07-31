2992a4eb3089df22ffd5a93b6cb11b796f61ff3b
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2026-08-01 00:32:11 +0700

    sprd_common_rw: Fix format specifiers in error messages

--- common/sprd_common_rw.c
+++ common/sprd_common_rw.c
@@ -139,8 +139,8 @@ int block_dev_write(block_dev_desc_t *dev_desc, uint64_t size, uint64_t offset, 
 
 	if (blkcnt > 0) {
 		if (blkcnt != dev_desc->block_write(dev_desc->dev, start, blkcnt, buf)) {
-			/*errorf("write block 0x%x size 0x%x fail\n", start, blkcnt);*/
-			errorf("write block 0x%lx size 0x%lx fail\n", start, blkcnt);
+			errorf("write block 0x%x size 0x%x fail\n", start, blkcnt);
+			/*errorf("write block 0x%lx size 0x%lx fail\n", start, blkcnt);*/
 			goto fail;
 		}
 		start += blkcnt;
@@ -731,7 +731,7 @@ int common_write_backstage(char *part_name, uint32_t size, uint64_t offset, char
 	int dev_id = 0;
 	lbaint_t start = 0;
 	lbaint_t blk_num = 0;
-	/*uint64_t total_part_size = 0;*/
+	uint64_t total_part_size = 0;
 	block_dev_desc_t *dev_desc = NULL;
 	int ret = 0;
 	char *ifname;
@@ -741,14 +741,14 @@ int common_write_backstage(char *part_name, uint32_t size, uint64_t offset, char
 	dev_id = get_devnum_hwpart(ifname, USER_PART);
 	dev_desc = get_dev_hwpart(ifname, dev_id, USER_PART);
 	if (0 != strcmp((const char *)part_name, (const char *)local_part_info.name)) {
-		/*ret = get_partition_info_by_name(dev_desc, part_name, &local_part_info);*/
-		ret = get_partition_info_by_name(dev_desc, (uchar *)part_name, &local_part_info);
+		ret = get_partition_info_by_name(dev_desc, part_name, &local_part_info);
+		/*ret = get_partition_info_by_name(dev_desc, (uchar *)part_name, &local_part_info);*/
 	}
 
 	/*invoker must guarantee that the size and offset aligned to the block size*/
 	if (0 != size % local_part_info.blksz || 0 != offset % local_part_info.blksz) {
-		/*errorf("input size or offset not aligned, size(0x%x), offset(0x%x)\n", size, offset);*/
-		errorf("input size or offset not aligned, size(0x%x), offset(0x%llx)\n", size, offset);
+		errorf("input size or offset not aligned, size(0x%x), offset(0x%x)\n", size, offset);
+		/*errorf("input size or offset not aligned, size(0x%x), offset(0x%llx)\n", size, offset);*/
 		ret = -1;
 	}
 
